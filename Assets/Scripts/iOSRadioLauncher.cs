81d61c5a75bf1c499f51985557424c546d70260e
MBP dev <mbpdev@unknown.home>
2025-04-27 10:03:36 +0100

    iOS Build 1.0.46(42): Auto-commit from Unity

--- Assets/Scripts/iOSRadioLauncher.cs
+++ Assets/Scripts/iOSRadioLauncher.cs
@@ -6,21 +6,21 @@ public class iOSRadioLauncher : MonoBehaviour
 {
     private float playbackTime;
 
-    //     void Update()
-    //     {
-    // #if UNITY_IOS && !UNITY_EDITOR
-    //         string state = iOSRadioLauncher.CheckiOSPlaybackState();
+        void Update()
+        {
+    #if UNITY_IOS && !UNITY_EDITOR
+            string state = iOSRadioLauncher.CheckiOSPlaybackState();
 
-    //         if (state == "PLAYING")
-    //         {
-    //             playbackTime += Time.deltaTime;
-    //         }
-    //         else if (state == "STOPPED" || state == "BUFFERING" || state == "ERROR")
-    //         {
-    //             playbackTime = 0f;
-    //         }
-    // #endif
-    //     }
+            if (state == "PLAYING")
+            {
+                playbackTime += Time.deltaTime;
+            }
+            else if (state == "STOPPED" || state == "BUFFERING" || state == "ERROR")
+            {
+                playbackTime = 0f;
+            }
+    #endif
+        }
 
     public string GetiOSPlaybackTime()
     {
