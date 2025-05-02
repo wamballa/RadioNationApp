74c2a2a0df21004bf98e772209f25bff10707dc2
MBP dev <mbpdev@MBPs-MacBook-Pro.local>
2025-05-02 13:47:05 +0100

    iOS Build 1.0.114(110): Auto-commit from Unity

--- Assets/Plugins/iOS/RadioStreamManager.mm
+++ Assets/Plugins/iOS/RadioStreamManager.mm
@@ -10,7 +10,7 @@ void fetchNowPlaying(NSString *urlStr);
 bool IsNetworkReachable(void);
 void setupRemoteCommands(void);
 void setupNetworkMonitor(void);
-void UpdateNowPlayingLockscreen(const char* title);
+//void UpdateNowPlayingLockscreen(const char* title);
 
 //extern "C" void StartStream(const char* url);  // ✅ CORRECT
 // extern "C" void StartStream(const char* url);
