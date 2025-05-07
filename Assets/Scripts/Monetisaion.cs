25290a21af780424804bdff79c9d6be85f05f257
MBP dev <mbpdev@mbps-mbp.home>
2025-05-07 15:14:46 +0100

    iOS Build 1.0.144(140): Auto-commit from Unity

--- Assets/Scripts/Monetisaion.cs
+++ Assets/Scripts/Monetisaion.cs
@@ -1,20 +1,13 @@
 using UnityEngine;
-//using Gley.MobileAds.Internal;
 using Gley.MobileAds;
-using static Gley.MobileAds.Events;
-using Gley.MobileAds.Internal;
+
 
 public class Monetisation : MonoBehaviour
 {
     public bool logToConsole = true;
-    public RectTransform bannerAnchor;
 
     private void Awake()
     {
-        Log("[Awake] IronSource Initialisation =============================================");
-
-        Log("[Awake] Device Model: " + SystemInfo.deviceModel);
-
         // TEST DEVICES - Get the GAID
         //IronSource.Agent.validateIntegration(); // ✅ OK on GAID-capable devices
         //string s = IronSource.Agent.getAdvertiserId();
@@ -22,14 +15,14 @@ public class Monetisation : MonoBehaviour
 
         API.Initialize(OnInitialized);
 
-        Log("[Awake] IronSource Initialisation =============================================");
+        Log("[Awake]  Initialisation =============================================");
 
     }
 
 
     void OnInitialized()
     {
-        Log("[OnInitialized] OnInitialized");
+        Log("[OnInitialized] OnInitialized =============================================");
         ShowBanner();
     }
 
@@ -45,7 +38,7 @@ public class Monetisation : MonoBehaviour
         //Vector2 screenPos = RectTransformUtility.WorldToScreenPoint(null, bannerAnchor.position);
         //Gley.MobileAds.API.ShowBanner((int)screenPos.x, (int)screenPos.y, BannerType.Banner);
         //API.ShowBanner(bannerPositionX:0, bannerPositionY:500, bannerType:BannerType.Banner);
-        Log("[ShowBanner] ShowBanner");
+        Log("[ShowBanner] ShowBanner =============================================");
 
         API.ShowBanner(BannerPosition.Bottom, BannerType.Banner);
     }
