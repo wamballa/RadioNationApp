76eeab7f3c9a281e47909af34bbf6b16ac51b17c
Steve T <steve.tyrell@gmail.com>
2025-05-09 08:55:06 +0100

    iOS Build 1.1.19(156): Auto-commit from Unity

--- Assets/Scripts/ShowVersion.cs
+++ Assets/Scripts/ShowVersion.cs
@@ -7,39 +7,41 @@ public class ShowVersion : MonoBehaviour
 
     void Start()
     {
-        versionText.text = $"Version {Application.version} ({GetBundleVersionCode()})";
-    }
+        // versionText.text = $"Version {Application.version} ({GetBundleVersionCode()})";
+        versionText.text = $"Version {Application.version}";
 
-    int GetBundleVersionCode()
-    {
-#if UNITY_ANDROID && !UNITY_EDITOR
-    using (var versionCodeClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
-    {
-        var context = versionCodeClass.GetStatic<AndroidJavaObject>("currentActivity");
-        var pm = context.Call<AndroidJavaObject>("getPackageManager");
-        var pkgName = context.Call<string>("getPackageName");
-        var pkgInfo = pm.Call<AndroidJavaObject>("getPackageInfo", pkgName, 0);
-        return pkgInfo.Get<int>("versionCode");
     }
-#elif UNITY_IOS && !UNITY_EDITOR
-    return int.Parse(GetiOSBuildNumber());
-#elif UNITY_EDITOR
-    return UnityEditor.PlayerSettings.iOS.buildNumber != null
-        ? int.Parse(UnityEditor.PlayerSettings.iOS.buildNumber)
-        : 0;
-#else
-        return 0;
-#endif
-    }
-
-#if UNITY_IOS && !UNITY_EDITOR
-[System.Runtime.InteropServices.DllImport("__Internal")]
-private static extern string _getCFBundleVersion();
-
-string GetiOSBuildNumber()
-{
-    return _getCFBundleVersion();
 }
-#endif
+//     int GetBundleVersionCode()
+//     {
+// #if UNITY_ANDROID && !UNITY_EDITOR
+//     using (var versionCodeClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+//     {
+//         var context = versionCodeClass.GetStatic<AndroidJavaObject>("currentActivity");
+//         var pm = context.Call<AndroidJavaObject>("getPackageManager");
+//         var pkgName = context.Call<string>("getPackageName");
+//         var pkgInfo = pm.Call<AndroidJavaObject>("getPackageInfo", pkgName, 0);
+//         return pkgInfo.Get<int>("versionCode");
+//     }
+// #elif UNITY_IOS && !UNITY_EDITOR
+//     return int.Parse(GetiOSBuildNumber());
+// #elif UNITY_EDITOR
+//     return UnityEditor.PlayerSettings.iOS.buildNumber != null
+//         ? int.Parse(UnityEditor.PlayerSettings.iOS.buildNumber)
+//         : 0;
+// #else
+//         return 0;
+// #endif
+//     }
 
-}
+// #if UNITY_IOS && !UNITY_EDITOR
+// [System.Runtime.InteropServices.DllImport("__Internal")]
+// private static extern string _getCFBundleVersion();
+
+// string GetiOSBuildNumber()
+// {
+//     return _getCFBundleVersion();
+// }
+// #endif
+
+// }
